1c09a86e21388b9ad8a93c04962173b625cc44fa
PC_G4600 <Armen.181@gmail.com>
2018-11-01 08:26:27 +0400

    Initial commit

---
+++ src/main/java/net/ddns/armen181/jokes/jokewebsite/JokewebsiteApplication.java
@@ -0,0 +1,12 @@
+package net.ddns.armen181.jokes.jokewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokewebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokewebsiteApplication.class, args);
+    }
+}
---
+++ src/main/java/net/ddns/armen181/jokes/jokewebsite/services/JokeService.java
@@ -0,0 +1,5 @@
+package net.ddns.armen181.jokes.jokewebsite.services;
+
+public interface JokeService {
+    String getJoke();
+}
